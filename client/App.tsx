5298b646d1aa845af23b5dc1e6a982f93237c7da
Justin Albert <Jnalbert879@gmail.com>
2021-07-12 12:05:47 -0700

    add styled comps

--- client/App.tsx
+++ client/App.tsx
@@ -1,21 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppContainer } from './src/styles/AppStyles';
+import { Text } from 'react-native';
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
+    <AppContainer>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </AppContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
