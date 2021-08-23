ef956dda1777ae78d328546a34108ccc7e8f4a0d
Justin Albert <Jnalbert879@gmail.com>
2021-08-22 21:45:28 -0700

    add media buttons

---
+++ client/src/components/AudioFileListen/MediaButtons.tsx
@@ -0,0 +1,54 @@
+import React, { FC } from 'react';
+import { TouchableOpacity, View, Image } from 'react-native';
+import styled from "styled-components/native"
+
+import { FontAwesome } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons'; 
+
+const MediaButtonsWrapper = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 60%;
+  margin-top: 35px;
+`
+
+const MediaButton = styled.View`
+  justify-content: center;
+  align-items: center;
+  padding-left: auto;
+  padding-right: auto;
+`
+
+interface MediaButtonsProps {
+  isPaused: boolean;
+  onPressMainButton: () => void;
+  traverse: (amount: number) => void;
+}
+
+const MediaButtons: FC<MediaButtonsProps> = ({isPaused, onPressMainButton, traverse}) => {
+  return (
+    <MediaButtonsWrapper>
+      <MediaButton>
+        <TouchableOpacity onPress={() => traverse(-10)}>
+          <Image source={require("../../../assets/replay-10.png")} />
+        </TouchableOpacity>
+      </MediaButton>
+
+      <MediaButton>
+        <TouchableOpacity onPress={onPressMainButton}>
+          {isPaused ? <FontAwesome style={{ transform: [{ translateX: 4 }]}} name="play" size={40} color="black" /> : <Fontisto name="pause" size={33} color="black" />}
+        </TouchableOpacity>
+      </MediaButton>
+
+      <MediaButton>
+        <TouchableOpacity  onPress={() => traverse(10)}>
+          <Image source={require("../../../assets/forward-10.png")} />
+        </TouchableOpacity>
+      </MediaButton>
+    </MediaButtonsWrapper>
+  )
+}
+
+export default MediaButtons
--- client/src/screens/main/AudioFileListenScreen.tsx
+++ client/src/screens/main/AudioFileListenScreen.tsx
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 import styled from "styled-components/native"
 import AudioSeeker from '../../components/AudioFileListen/AudioSeeker';
 import FileInfoComp from '../../components/AudioFileListen/FileInfoComp';
+import MediaButtons from '../../components/AudioFileListen/MediaButtons';
 import ScreenWrapperComp from '../../shared/ScreenWrapperComp';
 
 
@@ -37,8 +38,22 @@ const AudioFileListenScreen: FC<AudioFileListenScreen> = ({ route }) => {
   }
 
   const onSeekComplete = (time: number) => {
-    console.log(rounder(time))
     setCurrentPosition(rounder(time))
+  }
+
+  const onPressPlayPause = () => {
+    setIsPlaying(!isPlaying)
+  }
+
+  const traverseThrough = (amount: number) => {
+    let newAmount;
+    if (amount < 0) {
+      newAmount = Math.max(0, currentPosition + amount)
+    } else {
+      newAmount = Math.min(length, currentPosition + amount)
+    }
+
+    setCurrentPosition(newAmount)
   }
 
 
@@ -47,7 +62,7 @@ const AudioFileListenScreen: FC<AudioFileListenScreen> = ({ route }) => {
     <ScreenWrapperComp>
       <FileInfoComp imgData={imgUrl} header={header} description={description} />
       <AudioSeeker onValueChange={onValueChange} onSeekStart={onSeekStart} onSeekComplete={onSeekComplete} fileLengthSeconds={length} currentPositionSeconds={ currentPosition}/>
-        
+      <MediaButtons isPaused={!isPlaying} onPressMainButton={onPressPlayPause} traverse={traverseThrough}/>
     </ScreenWrapperComp>
   )
 }
